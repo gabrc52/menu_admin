b67400745e9b969cf36f17a4d853f0ec4bf86a79
Gabriel <gabrc52@gmail.com>
2022-08-11 21:20:00 -0400

    finish backend

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,20 +1,9 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as assert from 'assert'; /// I need a MODULE to assert?
 
 admin.initializeApp();
 const db = admin.firestore();
-
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!!1");
-// });
-
-// export const helloWorldJson = functions.https.onRequest((req, res) => {
-//   res.json({hello: "world"});
-// });
 
 export const fechas = functions.https.onRequest(async (req, res) => {
   const doc = await db.doc('/json/fechas').get();
@@ -39,6 +28,13 @@ export const updates = functions.https.onRequest(async (req, res) => {
   res.json(doc.data());
 });
 
+// https://stackoverflow.com/questions/563406/how-to-add-days-to-date
+function addDays(date : Date, days : number) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
 export const launchUpdate = functions.firestore.document("/updates/updates").onUpdate(async (change, context) => {
   const backup = async (type: string, change: functions.Change<functions.firestore.QueryDocumentSnapshot>) => {
     console.log(`updating ${type} ${change.before.get(type)} to ${change.after.get(type)}`);
@@ -46,12 +42,23 @@ export const launchUpdate = functions.firestore.document("/updates/updates").onU
     db.collection(`/json/${type}/backups`).add({str: old.get("str"), date: change.before.get(type)});
   };
 
-  const save = async (type: string, json: Object) => {
-    const jsonStr = JSON.stringify(json);
-    await db.doc('/json/fechas/').set({
+  const save = async (type: string, json: Object | string, date : string) => {
+    let jsonStr : string;
+    if (json instanceof Object) {
+      jsonStr = JSON.stringify(json); 
+    } else {
+      jsonStr = json;
+    }
+    await db.doc(`/json/${type}/`).set({
       'str': jsonStr,
-      'date': change.after.get('fechas'),
+      'date': date,
     });
+  };
+
+  const timestampToDate = (timestamp: admin.firestore.Timestamp): Date => {
+    const date : Date = new Date(timestamp.seconds * 1000);
+    console.log(timestamp, date);
+    return date;
   };
 
   /// Fechas
@@ -59,33 +66,69 @@ export const launchUpdate = functions.firestore.document("/updates/updates").onU
     await backup("fechas", change);
 
     const doc = await db.doc('/fechas/semestre').get();
-    const inicio = doc.get('inicio');
-    console.log(inicio);
-    // result is Timestamp { _seconds: 1648440000, _nanoseconds: 0 }
-    const fin = doc.get('fin');
-    console.log(fin);
-    const startingDay = doc.get('starting-day');
+    const inicio = timestampToDate(doc.get('inicio'));
+    const fin = timestampToDate(doc.get('fin'));
+    const startingDay : number = doc.get('starting-day');
+    assert(1 <= startingDay && startingDay <= 56, 'starting day must be between 1 and 56');
+
+    /// En la versión anterior de la app, no se permite establecer el día inicial
+    /// Entonces `inicio` sería la fecha necesaria para que en el inicio deseado inicie desde ese día
+    // https://stackoverflow.com/questions/563406/how-to-add-days-to-date
+    const inicioLegacy = addDays(inicio, -startingDay + 1);
+
     const fechas = {
       'inicio': {
-        
+        'year': inicioLegacy.getUTCFullYear(),
+        'month': inicioLegacy.getUTCMonth() + 1, // this one uses zero-indexing
+        'day': inicioLegacy.getUTCDate(), // yes, date, not day, fucking javascript
       },
       'inicio-v2': {
+        'year': inicio.getUTCFullYear(),
+        'month': inicio.getUTCMonth() + 1,
+        'day': inicio.getUTCDate(),
         'starting-day': startingDay,
       },
-      'fin': {},
+      'fin': {
+        'year': fin.getUTCFullYear(),
+        'month': fin.getUTCMonth() + 1,
+        'day': fin.getUTCDate(),
+      },
     };
 
-    await save('fechas', fechas);
+    await save('fechas', fechas, change.after.get('fechas'));
   }
 
   /// Info
   if (change.before.get("info") < change.after.get("info")) {
     await backup("info", change);
+
+    const info : any = {};
+    const avisos = await db.collection('info').listDocuments();
+    for (const avisoRef of avisos) {
+      const aviso : any = (await avisoRef.get()).data();
+      const date = aviso.date;
+      aviso.date = undefined;
+      if (aviso.icon == 58172 && aviso.icon_font == 'MaterialIcons') {
+        aviso.icon = undefined;
+        aviso.icon_font = undefined;
+      }
+      assert(info[date] === undefined, `aviso duplicado para fecha ${date}`);
+      info[date] = aviso;
+    }
+    console.log(info);
+    
+    await save('info', info, change.after.get('info'));
   }
 
   /// Menu
   if (change.before.get("menu") < change.after.get("menu")) {
     await backup("menu", change);
+    
+    const doc = await db.doc('/menu/json').get();
+    const menuJson : string = doc.get('rawData');
+    console.log(menuJson);
+
+    await save('menu', menuJson, change.after.get('menu'));
   }
 });
 
